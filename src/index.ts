96c5e451ca6e54aa95d2e612f0e1efad206ed7a5
Puyodead1 <puyodead@proton.me>
2022-12-22 18:37:35 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,18 @@ import { Injector, webpack } from "replugged";
 const inject = new Injector();
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      console.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
+  const mod = await webpack.waitForModule<{
+    fE: (args: any) => void;
+  }>(webpack.filters.bySource(/\w+=\w+\.shouldMention,\w+=\w+\.showMentionToggle/));
+  if (!mod) {
+    console.error("[NoReplyMention] Failed to find module, cannot continue!");
+    return;
   }
+
+  inject.before(mod, "fE", ([args], _) => {
+    args.shouldMention = false;
+    return args;
+  });
 }
 
 export function stop(): void {
