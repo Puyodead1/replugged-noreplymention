36987b6037fc3479af98edd8e17f1fac4135b6ff
Puyodead1 <puyodead@proton.me>
2022-12-22 18:57:40 -0500

    don't hardcode function name

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { Injector, webpack } from "replugged";
+import { AnyFunction } from "replugged/dist/types";
 
 const inject = new Injector();
 
 export async function start(): Promise<void> {
-  const mod = await webpack.waitForModule<{
-    fE: (args: any) => void;
-  }>(webpack.filters.bySource(/\w+=\w+\.shouldMention,\w+=\w+\.showMentionToggle/));
+  const mod = await webpack.waitForModule<{ [key: string]: AnyFunction }>(
+    webpack.filters.bySource(/\w+=\w+\.shouldMention,\w+=\w+\.showMentionToggle/),
+  );
   if (!mod) {
     console.error("[NoReplyMention] Failed to find module, cannot continue!");
     return;
   }
+  console.log(mod);
 
-  inject.before(mod, "fE", ([args], _) => {
+  const fnName = Object.entries(mod).find(([_, v]) =>
+    v.toString()?.match(/CREATE_PENDING_REPLY/),
+  )?.[0];
+  if (!fnName) {
+    console.error("[NoReplyMention] Failed to find function, cannot continue!");
+    return;
+  }
+
+  inject.before(mod, fnName, ([args], _) => {
     args.shouldMention = false;
     return args;
   });
