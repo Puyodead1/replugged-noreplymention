02901fe6e4680eede153e58cad2e7583be60fa6b
Puyodead1 <puyodead@proton.me>
2023-01-07 15:11:53 -0500

    Update to replugged beta20

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { Injector, webpack } from "replugged";
+import { Injector, Logger, webpack } from "replugged";
 import { AnyFunction } from "replugged/dist/types";
 
 const inject = new Injector();
 
 export async function start(): Promise<void> {
-  const mod = await webpack.waitForModule<{ [key: string]: AnyFunction }>(
+  const logger = Logger.plugin("NoReplyMention");
+
+  const injectionMod = await webpack.waitForModule<{ [key: string]: AnyFunction }>(
     webpack.filters.bySource(/\w+=\w+\.shouldMention,\w+=\w+\.showMentionToggle/),
   );
-  if (!mod) {
-    console.error("[NoReplyMention] Failed to find module, cannot continue!");
+  if (!injectionMod) {
+    logger.error("Failed to find module, cannot continue!");
     return;
   }
 
-  const fnName = Object.entries(mod).find(([_, v]) =>
+  const fnName = Object.entries(injectionMod).find(([_, v]) =>
     v.toString()?.match(/CREATE_PENDING_REPLY/),
   )?.[0];
   if (!fnName) {
-    console.error("[NoReplyMention] Failed to find function, cannot continue!");
+    logger.error("Failed to find function, cannot continue!");
     return;
   }
 
-  inject.before(mod, fnName, ([args], _) => {
+  inject.before(injectionMod, fnName, ([args], _) => {
     args.shouldMention = false;
     return args;
   });
